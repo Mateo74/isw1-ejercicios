cd78c84bb0d7720eaedd224b4d72b70c2bbf5fc1
Damianh97 <damianhuaier@gmail.com>
2019-08-27 19:28:26 -0300

    Clases Brazo y Suelo.

--- ToTheMoon-00/main.cpp
+++ ToTheMoon-00/main.cpp
@@ -1,4 +1,66 @@
-class Brazo {};
+enum class TipoDeSuelo
+{
+	DuroYCompacto,
+	BlandoYPoroso
+};
+
+class Suelo
+{
+public:
+	Suelo(int dureza, int porosidad): dureza(dureza), porosidad(porosidad) {}
+	
+	// Estos no se usan, estará bien sacarlos?
+	int dureza() const {return dureza;}
+	int porosidad() const {return porosidad;}
+	
+	TipoDeSuelo tipo() const
+	{
+		//Falta definir esto
+		if (...) {
+			return DuroYCompacto;
+		}
+		else if (...) {
+			return BlandoYPoroso;
+		}
+	}
+	
+private:
+	int dureza;
+	int porosidad;
+
+};
+
+enum class SentidoDeGiro
+{
+	SentidoHorario,
+	SentidoAntihorario
+};
+
+
+class Brazo
+{
+public:
+	void extraerMuestra(Suelo suelo)
+	{
+		if (suelo.tipo() == DuroYCompacto) {
+			girarMecha(SentidoHorario, 150, 10);
+			cerrarPinza();
+			girarMecha(SentidoAntihorario, 150, 10);
+		}
+		else if (suelo.tipo() == BlandoYPoroso) {
+			girarMecha(SentidoAntihorario, 100, 5);
+			cerrarPinza();
+			girarMecha(SentidoHorario, 100, 5);
+		}
+	}
+
+private:
+	void girarMecha(SentidoDeGiro sentido, int velocidad, int tiempo) {}
+	void cerrarPinza() {}
+
+};
+
+
 
 int main() {
 	return 0;
